16438241fc1e4397909dd7c63a2efb372d3feb06
Santi Susanti <43363256+saantisusanti@users.noreply.github.com>
2018-09-23 00:23:45 +0700

    Add files via upload

---
+++ PBO3_10117098_Latihan13_Relational/src/pbo3_10117098_latihan13_relational/PBO3_10117098_Latihan13_Relational.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117098_latihan13_relational;
+
+/**
+ * 
+ * @author 
+ * NAMA  : Santi Susanti
+ * KELAS : PBO3
+ * NIM   : 10117098
+ */
+public class PBO3_10117098_Latihan13_Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
